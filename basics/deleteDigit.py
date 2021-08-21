607946e13cd3e9830af8228b88c6c34f784693b7
JorG96 <89266161+JorG96@users.noreply.github.com>
2021-08-21 19:22:11 -0400

    Create deleteDigit.py

---
+++ basics/deleteDigit.py
@@ -0,0 +1,16 @@
+'''Given some integer, find the maximal number you can obtain by deleting exactly one digit of the given number.
+
+Example
+
+For n = 152, the output should be
+deleteDigit(n) = 52;
+For n = 1001, the output should be
+deleteDigit(n) = 101.'''
+
+from itertools import combinations
+def deleteDigit(n):
+    N=list(str(n))
+    a=[]
+    for i in list(combinations(N,len(N)-1)):
+        a.append(int("".join(i)))
+    return max(a)
